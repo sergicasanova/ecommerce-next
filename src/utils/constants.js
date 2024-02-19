a55a2c11748e086b20536c637e5f8dd5254df29a
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-02-19 14:19:41 +0100

    "constants"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-    SERVER_HOST: "http://localhost:1337",
-    API_URL: "http://localhost:1337/api",
+    SERVER_HOST: "https://ecommerce-strapi-production-a96f.up.railway.app/",
+    API_URL: "https://ecommerce-strapi-production-a96f.up.railway.app/api",
     ENDPOINTS: {
         AUTH: {
           REGISTER: "auth/local/register",
